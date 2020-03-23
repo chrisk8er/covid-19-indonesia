30fd5ab82a2c8c95e40f352aff109a23fdfab938
krestian <chrisk8er@gmail.com>
2020-03-23 17:16:51 +0700

    create redirect page to maintenance

---
+++ src/pages/maintenance.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+
+const NotFoundPage = () => (
+  <Layout>
+    <SEO title="301 Moved Permanently" />
+    <h1>Maintenance</h1>
+    <p>Our site are Maintenance... the sadness.</p>
+  </Layout>
+)
+
+export default NotFoundPage
