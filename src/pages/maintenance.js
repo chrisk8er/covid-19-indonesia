59c73ea2dd20a16652ad01daad84f70a3eb8f605
krestian <chrisk8er@gmail.com>
2020-03-23 18:12:52 +0700

    fixing the words and centering the content

--- src/pages/maintenance.js
+++ src/pages/maintenance.js
@@ -3,12 +3,25 @@ import React from 'react'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
-const NotFoundPage = () => (
+import Box from '@material-ui/core/Box'
+
+const MaintenancePage = () => (
   <Layout>
     <SEO title="301 Moved Permanently" />
-    <h1>Maintenance</h1>
-    <p>Our site are Maintenance... the sadness.</p>
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignContent="center"
+      mt={5}
+      mb={5}
+    >
+      <h1>Our site is getting a little tune up and some love.</h1>
+      <p>
+        We apologize for the inconvenience, but we're performing some
+        maintenance.
+      </p>
+    </Box>
   </Layout>
 )
 
-export default NotFoundPage
+export default MaintenancePage
